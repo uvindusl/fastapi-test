21ae1c1d958f412ba6dfbcd1fec24c9c0573083d
Uvindu Sankalpa <uvindusankalpa2021@gmail.com>
2025-09-07 00:38:38 +0530

    changes

--- alembic/versions/73efa4f44032_create_posts_table.py
+++ alembic/versions/73efa4f44032_create_posts_table.py
@@ -19,10 +19,10 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    op.create_table('posts', sa.Colunm(''))
+    op.create_table('posts', sa.Column('id', sa.Integer(), nullable=False,
+                    primary_key=True), sa.Column('title', sa.String(), nullable=False))
     pass
 
-
 def downgrade() -> None:
-    """Downgrade schema."""
+    op.drop_table('posts')
     pass
